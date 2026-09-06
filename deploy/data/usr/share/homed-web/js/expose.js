fa5f5fa6ee81c18da104ce3e6509cb1f1e7786ab
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2026-09-06 18:49:42 +0300

    Add support for media devices

--- deploy/data/usr/share/homed-web/js/expose.js
+++ deploy/data/usr/share/homed-web/js/expose.js
@@ -17,11 +17,15 @@ const defaultIcons =
     cover:             'window-shutter',
     fanMode:           'fan',
     heatMode:          'fire',
+    input:             'video-input-hdmi',
     level:             'brightness-6',
     light:             'lightbulb',
     lock:              'lock',
+    media:             'television',
+    mute:              'volume-off',
     operationMode:     'tune-variant',
     outlet:            'power-plug',
+    pause:             'pause',
     position:          'arrow-up-down',
     running:           'fire',
     swingMode:         'weather-windy',
@@ -265,6 +269,16 @@ function exposeList(expose, options)
         case 'switch':
             list = ['status'];
             break;
+
+        case 'media':
+        {
+            let controls = options[meta.id ? 'media_' + meta.id : 'media'];
+            let types = {volume: 'number', input: 'select', mute: 'toggle', pause: 'toggle'};
+
+            list = ['status'].concat(Array.isArray(controls) ? controls : new Array());
+            list.forEach(item => { if (types[item]) options[item] = {...options[item], type: types[item]}; });
+            break;
+        }
 
         case 'thermostat':
         {
